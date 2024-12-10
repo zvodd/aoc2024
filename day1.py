1ac408fbc6477ac27064fdfa653330a7a56fefb3
zvodd <zv.odd.101@gmail.com>
2024-12-10 21:56:19 +0800

    day1 part2

--- day1.py
+++ day1.py
@@ -8,14 +8,23 @@ with open(sys.argv[1]) as infile:
         va.append(int(a))
         vb.append(int(b))
         
-va.sort()
-vb.sort()
+# va.sort()
+# vb.sort()
 
-diffs = []
-for a,b in zip(va,vb):
-    diffs.append(abs(a - b))
+# diffs = []
+# for a,b in zip(va,vb):
+#     diffs.append(abs(a - b))
 
 #pprint(dict(zip([*zip(va,vb)], diffs)))
+#print(sum(diffs))
 
+### part 2
 
-print(sum(diffs))
+tallys = [vb.count(lv) for lv in va]
+#pprint(dict(zip(va, tallys)))
+
+score = 0
+for v,m in zip(va, tallys):
+    score += v * m
+
+print(score)
