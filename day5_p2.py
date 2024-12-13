7e198d26fb1dc15a730f18b2c536325145dd415b
zvodd <zv.odd.101@gmail.com>
2024-12-13 22:29:13 +0800

    day5 p2

--- day5_p2.py
+++ day5_p2.py
@@ -3,6 +3,8 @@ def parse(lines):
     seqs = []
     state = 0
     for i,line in enumerate(lines):
+        if line.lstrip().startswith("#"):
+            continue
         if line == "":
             state += 1
             continue
@@ -25,29 +27,62 @@ def main():
     for i, r in enumerate(rules):
         if rulemap.get(r[0], None) is None:
             therest = [rules[j][1] for j in range(i,len(rules)) if rules[j][0] == r[0]]
-            #therest.sort()
             rulemap[r[0]] = therest
 
     #print(rulemap)
     #print(print_seqs)
-    
 
     ok_jobs = []
     nok_jobs = []
     for job in print_seqs:
         valid = True
-        for i_pages, page_no in enumerate(job):
-            prempts = rulemap.get(page_no, [])
-            for item in job[:i_pages]:
-                if item in rulemap.get(page_no, []):
+        for page_index, page in enumerate(job):
+            prempts = rulemap.get(page, [])
+            for item in job[:page_index]:
+                if item in rulemap.get(page, []):
                     valid = False
+                    break
         if valid:
             ok_jobs.append(job)
         else:
             nok_jobs.append(job)
-    #print(ok_jobs)
+    
+    fixed_jobs = []
+    for seq in nok_jobs:
+        page_index = 0
+        sorted_seq = seq.copy()
+    
+        while page_index < len(sorted_seq):
+            page = sorted_seq[page_index]
 
+            sentinal_index = len(seq) + 1
+            earliest_index = sentinal_index
 
+            # Find the earliest index of post_page
+            for post_page in rulemap.get(page, []):
+                try:
+                    found = sorted_seq.index(post_page)
+                    if found < earliest_index:   
+                        earliest_index = found
+                except ValueError:
+                    continue
+            
+            # If a valid earlier index is found
+            if earliest_index != sentinal_index:
+                current_page = sorted_seq.pop(page_index)
+                sorted_seq.insert(earliest_index, current_page)
+            else:
+                page_index += 1
+        
+        fixed_jobs.append(sorted_seq)
+
+    print("rulemap:".ljust(15, " ") + f"{rulemap}")
+    print("#---------------------#")
+    print("nok_jobs:".ljust(15, " ") + f"{nok_jobs}")
+    print("fixed_jobs:".ljust(15, " ") + f"{fixed_jobs}")
+    # print("ok_jobs:".ljust(15, " ") + f"{ok_jobs}")
+
+    print("#---------------------#\n")
 
     middle_values = [j[len(j) // 2] for j in fixed_jobs]
     print(sum(middle_values))
